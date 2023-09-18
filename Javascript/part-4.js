daabd3120cca3d5ffa0fb6d33f340a6e2fc592d9
Sankarsan Dehury <140236047+sankarsandehury@users.noreply.github.com>
2023-09-18 18:02:17 +0000

    container config done

---
+++ Javascript/part-4.js
@@ -0,0 +1,8 @@
+const accountId=144553;
+let accountEmail='bapi@gmail.com';
+var accountPassword='12546';
+accountCity='Odisha';
+accountEmail='dehury@gmail.com';
+accountPassword='245687';
+console.log(accountId);
+console.table([accountCity,accountPassword,accountEmail]);
