96191fb55f915763d39eaea4d68e86ed81f31aec
Sankarsan Dehury <140236047+sankarsandehury@users.noreply.github.com>
2023-09-20 07:02:11 +0000

    datatype convertion

---
+++ Javascript/part-7.js
@@ -0,0 +1,20 @@
+let value =3;
+let negvalue= -value;
+console.log(negvalue)
+
+let str1='shree'
+let str2 ='ram';
+// console.log(str1+str2)
+
+// console.log("1"+2);
+// console.log(1+'2');
+// console.log("1"+2+2);
+// console.log(1+2+'2');
+
+console.log(+true);
+// console.log(true+);
+
+let game=100;
+//game++;
+++game
+console.log(game);
