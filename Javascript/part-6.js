b1572c6912d94c251b904b72b2000e818959afde
Sankarsan Dehury <140236047+sankarsandehury@users.noreply.github.com>
2023-09-20 06:13:07 +0000

    convertion of datatype

---
+++ Javascript/part-6.js
@@ -0,0 +1,24 @@
+let score ="33";
+console.log(typeof score);
+console.log(typeof (score));
+
+let value=Number(score);
+console.log(typeof value);
+
+//"33" => 33
+//"33abc" => NaN
+//true=>1 false=>0
+
+
+let isLogged=1
+let booleanlog =Boolean(isLogged);
+console.log(booleanlog);
+
+//1=>true
+//0=>false
+//""=>false
+//"bapi"=>true
+
+let some= 33;
+let stringnumber=String(some)
+console.log(some)
