a7a7cfa0dd2463098dcb43906bbad4f713590ae0
Sankarsan Dehury <140236047+sankarsandehury@users.noreply.github.com>
2023-09-20 18:50:11 +0000

    Date & Time

---
+++ Javascript/part-13.js
@@ -0,0 +1,24 @@
+// Date Object
+let mydate=new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(typeof mydate);
+
+// let myCreatedDate=new Date(2023,8,24);
+// let myCreatedDate=new Date(2023,8,24,5,3)
+let myCreatedDate=new Date("2023-01-14");
+// console.log(myCreatedDate.toLocaleString());
+// console.log(myCreatedDate.getTime());
+
+let myTimeStamp=Date.now()
+// let myTimeStamp=new Date()
+// console.log(myTimeStamp);
+// console.log(Math.floor(Date.now()/1000)); second convert
+
+
+let newdate=new Date();
+// console.log(newdate);
+// console.log(newdate.getMonth());
+// console.log(newdate.getDate());
+
