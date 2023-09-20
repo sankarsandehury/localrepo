b9fe94ca984921b83029302f83a31e24e424d99f
Sankarsan Dehury <140236047+sankarsandehury@users.noreply.github.com>
2023-09-20 07:50:34 +0000

    Datatype summary

---
+++ Javascript/part-9.js
@@ -0,0 +1,22 @@
+// primitive--> 7types:string,number,boolean,null,undefined,symbol,Bigint
+
+const score=100;
+
+const islogged=false;
+const outside=null;
+let usermail;
+
+const id =Symbol('123')
+const another=Symbol('123')
+console.log(id===another);
+
+//Reference (Non primitive) --> Array,object,function
+const heros=['ram','sam','hitesh'];
+let myobj={
+   name:'bapi',
+   age:24
+}
+
+const Myfunction= function () {
+   console.log('jay shree ram');
+}
